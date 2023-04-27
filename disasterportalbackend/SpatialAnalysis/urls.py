07561e5b28f94b0d7f0bfeb33a77cb4202521da3
cecil <cecilghimire@gmail.com>
2023-04-27 09:57:25 +0545

    fix

--- disasterportalbackend/SpatialAnalysis/urls.py
+++ disasterportalbackend/SpatialAnalysis/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 from SpatialAnalysis.views import *
 router = routers.DefaultRouter()
 router.register(r'allbuilding', BuildingViewset)
+
 urlpatterns = [
     path('', include(router.urls)),
     path("building/", BufferPolygonIntersectionViewBuilding.as_view()),
@@ -16,4 +17,7 @@ urlpatterns = [
     path('nearest_forests/<str:latitude>/<str:longitude>/<int:distance>/', NearestForests.as_view()),
     path('nearest_roads/<str:latitude>/<str:longitude>/<int:distance>/', NearestRoads.as_view()),
     path('nearest_waterbody/<str:latitude>/<str:longitude>/<int:distance>/', NearestWaterBody.as_view()),
+    path('singlebuilding/',SingleBuildingViewset.as_view(),name='single-building'),
+    path('email/',BufferPolygonIntersectionViewBuildingTriggerAlert.as_view(),name='alert-email'),
+    path('patchbuilding/', building_partial_update,name='patch building')
 ]
